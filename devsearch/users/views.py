e3c9fd093d5ffb78825a7954b925fba1c7ff0a64
Davraph <rapheal98@icloud.com>
2022-09-18 22:48:44 +0200

    added messaging functionalit

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -2,9 +2,9 @@
 from distutils.sysconfig import customize_compiler
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from .models import Profile, Skill
+from .models import Profile, Skill, Message
 from django.contrib import messages
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
@@ -67,7 +67,7 @@ def registerUser(request):
 
     context = {
         'page': page,
-        'form': form
+        'form': form,
     }
     return render(request, 'users/login_register.html', context)
 
@@ -166,7 +166,7 @@ def updateSkill(request, pk):
 
 
 
-
+@login_required(login_url='login')
 def deleteSkill(request, pk):
     profile = request.user.profile
     skill = profile.skill_set.get(id=pk)
@@ -179,3 +179,63 @@ def deleteSkill(request, pk):
         'object': skill
     }
     return render(request, 'delete_template.html', context)
+
+
+@login_required(login_url='login')
+def inbox(request):
+    profile = request.user.profile
+    messageRequests = profile.messages.all()
+    
+    unreadCount = messageRequests.filter(is_read=False).count()
+
+    context = {
+        'messageRequests': messageRequests,
+        'unreadCount': unreadCount ,
+
+    }
+    return render(request, 'users/inbox.html', context)
+
+@login_required(login_url='login')
+def viewMessage(request, pk):
+    profile = request.user.profile
+    message = profile.messages.get(id=pk)
+    if message.is_read == False: 
+        message.is_read = True
+        message.save()
+
+    context = {
+        'message': message,
+    }
+    return render(request, 'users/message.html', context)
+
+
+@login_required(login_url='login')
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+    
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+
+            messages.success(request, 'Your Message was successfully sent')
+            return redirect('user-profile', pk=recipient.id)   
+
+    context = {
+        'recipient': recipient,
+        'form': form,
+    }
+    return render(request, 'users/message_form.html', context)
